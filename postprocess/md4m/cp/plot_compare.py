3fa46cdf8903a25d306f8c44bf650f0fe8a7c600
PENGYAN777 <peng.yan@mail.polimi.it>
2024-02-28 09:15:56 +0100

    md4m

--- postprocess/md4m/cp/plot_compare.py
+++ postprocess/md4m/cp/plot_compare.py
@@ -28,9 +28,9 @@ os.system('clear')
 
 mesh0 = pd.read_csv("m0new.csv", ",", skiprows=0)
 # mesh1 = pd.read_csv("m1new.csv", ",", skiprows=0)
-mesh2 = pd.read_csv("m2new.csv", ",", skiprows=0)
-mesh3 = pd.read_csv("m3new.csv", ",", skiprows=0)
-mesh4 = pd.read_csv("m4new.csv", ",", skiprows=0)
+# mesh2 = pd.read_csv("m2new.csv", ",", skiprows=0)
+# mesh3 = pd.read_csv("m3new.csv", ",", skiprows=0)
+# mesh4 = pd.read_csv("m4new.csv", ",", skiprows=0)
 
 
 
@@ -39,17 +39,17 @@ mesh4 = pd.read_csv("m4new.csv", ",", skiprows=0)
 
 
 
-Pc = 953950
-dc = 269
-Tc = 628.0
+Pc = 828558
+dc = 261.6
+Tc = 653.2
 # fig 1
 fig1 = plt.figure( dpi=300)
 lwh = 2
 axes = fig1.add_axes([0.15, 0.15, 0.7, 0.7]) #size of figure
 axes.plot(mesh0.iloc[:,-6] , mesh0.iloc[:,7]/Pc, 'k', lw=lwh, label="5k")
-axes.plot(mesh2.iloc[:,-6] , mesh2.iloc[:,7]/Pc, 'r', lw=lwh, label="12k")
-axes.plot(mesh3.iloc[:,-6] , mesh3.iloc[:,7]/Pc, 'g', lw=lwh, label="18k")
-axes.plot(mesh4.iloc[:,-6] , mesh4.iloc[:,7]/Pc, 'b', lw=lwh, label="30k")
+# axes.plot(mesh2.iloc[:,-6] , mesh2.iloc[:,7]/Pc, 'r', lw=lwh, label="12k")
+# axes.plot(mesh3.iloc[:,-6] , mesh3.iloc[:,7]/Pc, 'g', lw=lwh, label="18k")
+# axes.plot(mesh4.iloc[:,-6] , mesh4.iloc[:,7]/Pc, 'b', lw=lwh, label="30k")
 axes.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
 # sub_axes = plt.axes([0.26, 0.26, 0.25, 0.25]) 
 
@@ -70,10 +70,10 @@ fig2 = plt.figure( dpi=300)
 lwh = 2
 axes = fig2.add_axes([0.15, 0.15, 0.7, 0.7]) #size of figure
 axes.plot(mesh0.iloc[:,-6] , mesh0.iloc[:,3], 'k', lw=lwh, label="5k")
-axes.plot(mesh2.iloc[:,-6] , mesh2.iloc[:,3], 'r', lw=lwh, label="12k")
-axes.plot(mesh3.iloc[:,-6] , mesh3.iloc[:,3], 'g', lw=lwh, label="18k")
-axes.plot(mesh4.iloc[:,-6] , mesh4.iloc[:,3], 'b', lw=lwh, label="30k")
-axes.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+# axes.plot(mesh2.iloc[:,-6] , mesh2.iloc[:,3], 'r', lw=lwh, label="12k")
+# axes.plot(mesh3.iloc[:,-6] , mesh3.iloc[:,3], 'g', lw=lwh, label="18k")
+# axes.plot(mesh4.iloc[:,-6] , mesh4.iloc[:,3], 'b', lw=lwh, label="30k")
+# axes.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
 
 
 axes.set_xlabel('$X[mm]$',fontsize=12)
@@ -88,9 +88,9 @@ fig3 = plt.figure( dpi=300)
 lwh = 2
 axes = fig3.add_axes([0.15, 0.15, 0.7, 0.7]) #size of figure
 axes.plot(mesh0.iloc[:,-6] , mesh0.iloc[:,1]/dc, 'k', lw=lwh, label="5k")
-axes.plot(mesh2.iloc[:,-6] , mesh2.iloc[:,1]/dc, 'r', lw=lwh, label="12k")
-axes.plot(mesh3.iloc[:,-6] , mesh3.iloc[:,1]/dc, 'g', lw=lwh, label="18k")
-axes.plot(mesh4.iloc[:,-6] , mesh4.iloc[:,1]/dc, 'b', lw=lwh, label="30k")
+# axes.plot(mesh2.iloc[:,-6] , mesh2.iloc[:,1]/dc, 'r', lw=lwh, label="12k")
+# axes.plot(mesh3.iloc[:,-6] , mesh3.iloc[:,1]/dc, 'g', lw=lwh, label="18k")
+# axes.plot(mesh4.iloc[:,-6] , mesh4.iloc[:,1]/dc, 'b', lw=lwh, label="30k")
 
 
 axes.set_xlabel('$X[mm]$',fontsize=12)
@@ -105,20 +105,20 @@ fig4 = plt.figure( dpi=300)
 lwh = 2
 axes = fig4.add_axes([0.15, 0.15, 0.7, 0.7]) #size of figure
 axes.plot(mesh0.iloc[:,-6] , mesh0.iloc[:,-3], 'k', lw=lwh, label="5k")
-axes.plot(mesh2.iloc[:,-6] , mesh2.iloc[:,-3], 'r', lw=lwh, label="12k")
-axes.plot(mesh3.iloc[:,-6] , mesh3.iloc[:,-3], 'g', lw=lwh, label="18k")
-axes.plot(mesh4.iloc[:,-6] , mesh4.iloc[:,-3], 'b', lw=lwh, label="30k")
+# axes.plot(mesh2.iloc[:,-6] , mesh2.iloc[:,-3], 'r', lw=lwh, label="12k")
+# axes.plot(mesh3.iloc[:,-6] , mesh3.iloc[:,-3], 'g', lw=lwh, label="18k")
+# axes.plot(mesh4.iloc[:,-6] , mesh4.iloc[:,-3], 'b', lw=lwh, label="30k")
 
-sub_axes = plt.axes([0.55, 0.25, 0.25, 0.25]) 
-x = []
-# plot the zoomed portion
-for i in mesh4.iloc[:,-3].index:
-    if (mesh4.iloc[i,-3])<0:
-        x.append(i)
-sub_axes.plot(mesh4.iloc[x[0]:x[-1],-6], mesh4.iloc[x[0]:x[-1],-3] , 'b') 
-sub_axes.set_ylim([-0.007, -0.002])
-sub_axes.set_xlim([0, 0.4])
-sub_axes.yaxis.set_major_formatter(FormatStrFormatter('%.3f'))
+# sub_axes = plt.axes([0.55, 0.25, 0.25, 0.25]) 
+# x = []
+# # plot the zoomed portion
+# for i in mesh4.iloc[:,-3].index:
+#     if (mesh4.iloc[i,-3])<0:
+#         x.append(i)
+# sub_axes.plot(mesh4.iloc[x[0]:x[-1],-6], mesh4.iloc[x[0]:x[-1],-3] , 'b') 
+# sub_axes.set_ylim([-0.007, -0.002])
+# sub_axes.set_xlim([0, 0.4])
+# sub_axes.yaxis.set_major_formatter(FormatStrFormatter('%.3f'))
 
 axes.set_xlabel('$X[mm]$',fontsize=12)
 axes.set_ylabel('$\\Gamma$',fontsize=12) 
@@ -132,18 +132,28 @@ fig5 = plt.figure( dpi=300)
 lwh = 2
 axes = fig5.add_axes([0.15, 0.15, 0.7, 0.7]) #size of figure
 axes.plot(mesh0.iloc[:,-6] , mesh0.iloc[:,-1], 'k', lw=lwh, label="5k")
-axes.plot(mesh2.iloc[:,-6] , mesh2.iloc[:,-1], 'r', lw=lwh, label="12k")
-axes.plot(mesh3.iloc[:,-6] , mesh3.iloc[:,-1], 'g', lw=lwh, label="18k")
-axes.plot(mesh4.iloc[:,-6] , mesh4.iloc[:,-1], 'b', lw=lwh, label="30k")
 
-
-axes.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+# axes.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
 axes.set_xlabel('$X[mm]$',fontsize=12)
 axes.set_ylabel('s',fontsize=12) 
 axes.set_title('entropy along y$=0.25 mm$',fontsize=14)
 axes.legend(loc=1) # 
 
 fig5.savefig("shock_gv_s.pdf")
+
+# fig 6
+fig6 = plt.figure( dpi=300)
+lwh = 2
+axes = fig6.add_axes([0.15, 0.15, 0.7, 0.7]) #size of figure
+axes.plot(mesh0.iloc[:,-6] , 1-mesh0.iloc[:,-3]-1/mesh0.iloc[:,3]/mesh0.iloc[:,3], 'k', lw=lwh, label="5k")
+
+# axes.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+axes.set_xlabel('$X[mm]$',fontsize=12)
+axes.set_ylabel('J',fontsize=12) 
+# axes.set_title('entropy along y$=0.25 mm$',fontsize=14)
+axes.legend(loc=1) # 
+
+
 
 
 
